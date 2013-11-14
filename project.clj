abce38935a2e788548de76173be7177608d7c15f
Phillip Lord <phillip.lord@newcastle.ac.uk>
2013-11-14 14:39:43 +0000

    1.0 Tawny Upgrade.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject pizza "1.0.0-SNAPSHOT"
   :description "The Pizza Ontology in tawny-owl"
   :dependencies [
-                 [uk.org.russet/tawny-owl "1.0-rc2-SNAPSHOT"]
+                 [uk.org.russet/tawny-owl "1.0"]
                  ]
 
   :main pizza.core
